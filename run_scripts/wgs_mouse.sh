ccde9e6d165f4d538c1b10a654cbe31658ca9638
sam-widmayer <samuel.widmayer@jax.org>
2026-01-08 10:53:39 -0500

    fix wgs_sv subworkflow and wgs run script

--- run_scripts/wgs_mouse.sh
+++ run_scripts/wgs_mouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --mail-user=samuel.widmayer@jax.org
-#SBATCH --job-name=wgs_mouse_WMGP
+#SBATCH --mail-user=first.last@jax.org
+#SBATCH --job-name=wgs_mouse
 #SBATCH --mail-type=END,FAIL
 #SBATCH -p compute
 #SBATCH -q batch
@@ -18,17 +18,9 @@ module load nextflow/24.10.6
 nextflow ../main.nf \
 --workflow wgs \
 -profile sumner2 \
+--sample_folder <PATH_TO_YOUR_SEQUENCES> \
 --gen_org mouse \
---genome_build 'GRCm39' \
---ref_fa "/projects/compsci/vmp/USERS/widmas/1k_wild_mouse_wgs/metadata/GRCm39_Y_PAR_masked/sequence/GRCm39_masked.fa" \
---ref_fa_indices "/projects/compsci/vmp/USERS/widmas/1k_wild_mouse_wgs/metadata/GRCm39_Y_PAR_masked/sequence/GRCm39_masked.fa.fai" \
---ref_fa_dict "/projects/compsci/vmp/USERS/widmas/1k_wild_mouse_wgs/metadata/GRCm39_Y_PAR_masked/combined_ref_set/GRCm39_masked.dict" \
---csv_input "/projects/compsci/vmp/USERS/widmas/1k_wild_mouse_wgs/metadata/20251211_wgs_from_bam_manifest.csv" \
---bam_input true \
---deepvariant false \
---run_sv true \
---run_gvcf true \
---pubdir "/flashscratch/widmas/wmgp_gatk_sv" \
--w "/flashscratch/widmas/wmgp_gatk_sv_mito/work" \
---comment "This script will run whole genome sequencing analysis on mouse samples using default mm10" \
--resume
+--genome_build 'GRCm38' \
+--pubdir "/flashscratch/${USER}/outputDir" \
+-w "/flashscratch/${USER}/outputDir/work" \
+--comment "This script will run whole genome sequencing analysis on mouse samples using default mm10"
