bdc73ecf98048e35f270c190bbbae426b48673c8
Mike Lloyd <mike.lloyd@jax.org>
2026-04-28 14:04:57 -0400

    add flexibility to gen script, and add nf-test case

--- bin/generate_rnaseq_simreads/generate_simulated_RNA_data.R
+++ bin/generate_rnaseq_simreads/generate_simulated_RNA_data.R
@@ -15,124 +15,105 @@
 # 2026-03-11
 ################################################################################
 
-
-
 ##### LIBRARIES #####
 
 library(Rsubread)
-library(dplyr)
-library(ggplot2)
-library(tidyr)
-library(stringr)
-
-
 
 ## set command line arguments ----
 args <- commandArgs(trailingOnly = TRUE)
 
-# Wrapped fasta file : Mus_musculus.GRCm38.cds.all.LINEWRAP.fa
-wrap_fa = args[1]
-# gen_org = args[2]
+if (length(args) < 10) {
+  stop(
+    "Usage: Rscript generate_simulated_RNA_data.R <wrapped_fasta.fa> <library_size[,library_size...]> <PE|SE|BOTH> <read_length> <fragment_length_min> <fragment_length_max> <fragment_length_mean> <fragment_length_sd> <simulate_sequencing_error> <quality_reference>",
+    call. = FALSE
+  )
+}
 
-suffix   = strsplit(wrap_fa, '.', fixed = TRUE)[[1]][1]
-suffix_2 = strsplit(wrap_fa, '.', fixed = TRUE)[[1]][2]
+# Wrapped fasta file : Mus_musculus.GRCm38.cds.all.LINEWRAP.fa
+wrap_fa <- args[1]
+
+# Accept one value (e.g. 1000000) or comma-separated values (e.g. 1000000,30000000)
+library_size <- as.numeric(trimws(strsplit(args[2], ",", fixed = TRUE)[[1]]))
+if (any(is.na(library_size)) || any(library_size <= 0)) {
+  stop("library_size must be a positive integer or comma-separated positive integers.", call. = FALSE)
+}
+
+library_strategy <- toupper(trimws(args[3]))
+if (!(library_strategy %in% c("PE", "SE", "BOTH"))) {
+  stop("library_strategy must be one of: PE, SE, BOTH", call. = FALSE)
+}
+
+read_length <- as.numeric(args[4])
+fragment_length_min <- as.numeric(args[5])
+fragment_length_max <- as.numeric(args[6])
+fragment_length_mean <- as.numeric(args[7])
+fragment_length_sd <- as.numeric(args[8])
+simulate_sequencing_error <- as.logical(args[9])
+quality_reference <- args[10]
+
+
+suffix <- strsplit(wrap_fa, ".", fixed = TRUE)[[1]][1]
+suffix_2 <- strsplit(wrap_fa, ".", fixed = TRUE)[[1]][2]
 
 # Scan through the fasta file to get transcript names and lengths
 transcripts <- scanFasta(wrap_fa)
 nsequences <- nrow(transcripts) - sum(transcripts$Duplicate)
 
-
 # Assign a random TPM value to each non-duplicated transcript sequence
 TPMs <- rep(0, nrow(transcripts))
 TPMs[!transcripts$Duplicate] <- rexp(nsequences) # those that are duplicated are given '0' TPM values. 
 
+read_label <- function(size_value) {
+  if (size_value %% 1000000 == 0) {
+    paste0(size_value / 1000000, "m")
+  } else {
+    as.character(size_value)
+  }
+}
 
-# PE
-lib_size = c(1000000, 30000000) 
+run_simulation <- function(size_value, paired_end_value) {
+  reads <- read_label(size_value)
 
-for(i in seq_along(lib_size)) {
-
-  print(lib_size[i])
-
-  if(lib_size[i] == 1000000){
-    reads = '1m'
-  } else
-    reads = '30m'
-
-  print(reads)
-
-  output   = paste0(suffix, '_simRNA_', reads,'Reads') 
-  output_2 = paste0(suffix, '.', suffix_2, '.', reads,'_isoform_true_expression.txt') 
-
-  print(output)
-  print(output_2)
-  print(wrap_fa)
-  print(lib_size[i])  
+  if (paired_end_value) {
+    output <- paste0(suffix, "_simRNA_", reads, "Reads")
+    output_2 <- paste0(suffix, ".", suffix_2, ".", reads, "_isoform_true_expression.txt")
+  } else {
+    output <- paste0(suffix, "_simRNA_", reads, "Reads_SE")
+    output_2 <- paste0(suffix, ".", suffix_2, ".", reads, "_isoform_SE_true_expression.txt")
+  }
 
   true.counts <- simReads(
 
     # the transcript database and the wanted abundances
-    wrap_fa,TPMs, output, 
+    wrap_fa, TPMs, output,
     # options on the output
-    library.size = lib_size[i],
-    read.length = 100,
+    library.size = size_value,
+    read.length = read_length,
     truth.in.read.names = FALSE,
-  
+
     # simulating sequencing errors
-    simulate.sequencing.error = TRUE,
-    quality.reference = NULL,
-  
+    simulate.sequencing.error = simulate_sequencing_error,
+    quality.reference = if (simulate_sequencing_error && !quality_reference %in% c("none", "", NA)) quality_reference else NULL,
+
     # parameters for generating paired-end reads.
-    paired.end = TRUE,
-    fragment.length.min = 125,
-    fragment.length.max = 500,
-    fragment.length.mean = 250,
-    fragment.length.sd = 40,
-  
+    paired.end = paired_end_value,
+    fragment.length.min = fragment_length_min,
+    fragment.length.max = fragment_length_max,
+    fragment.length.mean = fragment_length_mean,
+    fragment.length.sd = fragment_length_sd,
+
     # manipulating transcript names
-    simplify.transcript.names = FALSE)
+    simplify.transcript.names = FALSE
+  )
 
-write.table(x = true.counts, file = output_2, sep = '\t', quote = F, row.names = F)
-
+  write.table(x = true.counts, file = output_2, sep = "\t", quote = FALSE, row.names = FALSE)
 }
 
-
-# SE for reads 1m
-reads = '1m'
-
-output   = paste0(suffix, '_simRNA_', reads,'Reads_SE') 
-output_2 = paste0(suffix, '.', suffix_2, '.', reads,'_isoform_SE_true_expression.txt') 
-
-print(output)
-print(output_2)
-print(wrap_fa)
-
-true.counts <- simReads(
-
-    # the transcript database and the wanted abundances
-    wrap_fa,TPMs, output,
-    # options on the output
-    library.size = lib_size[1],
-    read.length = 100,
-    truth.in.read.names = FALSE,
-
-    # simulating sequencing errors
-    simulate.sequencing.error = TRUE,
-    quality.reference = NULL,
-
-    # parameters for generating paired-end reads.
-    paired.end = FALSE,
-    fragment.length.min = 125,
-    fragment.length.max = 500,
-    fragment.length.mean = 250,
-    fragment.length.sd = 40,
-
-    # manipulating transcript names
-    simplify.transcript.names = FALSE)
-
-write.table(x = true.counts, file = output_2, sep = '\t', quote = F, row.names = F)
-
-
-
-
-
+for (size_value in library_size) {
+  if (library_strategy %in% c("PE", "BOTH")) {
+    run_simulation(size_value = size_value, paired_end_value = TRUE)
+  }
+  if (library_strategy %in% c("SE", "BOTH")) {
+    run_simulation(size_value = size_value, paired_end_value = FALSE)
+  }
+}
--- supportworkflows/generate_rnaseq_simreads.nf
+++ supportworkflows/generate_rnaseq_simreads.nf
@@ -4,6 +4,7 @@ nextflow.enable.dsl=2
 // import modules
 include {help} from "${projectDir}/bin/help/generate_rnaseq_simreads"
 include {param_log} from "${projectDir}/bin/log/generate_rnaseq_simreads"
+include {final_run_report} from "${projectDir}/bin/shared/final_run_report.nf"
 include {REFORMAT_FASTA} from "${projectDir}/modules/bbmap/bbmap_reformat_fasta"
 include {GENERATE_SIMULATED_RNA_DATA} from "${projectDir}/modules/r/generate_simulated_RNA_data"
 
@@ -28,11 +29,16 @@ def checkFileExists(filePath, name) {
     }
 }
 
-
 workflow GENERATE_RNASEQ_SIMREADS {
 
     REFORMAT_FASTA(params.fa_cds)
-    GENERATE_SIMULATED_RNA_DATA(REFORMAT_FASTA.out.wrap_fasta)
+
+    library_size = params.library_size instanceof String 
+        ? Channel.from(params.library_size.split(',').collect{it.trim().toInteger()})
+        : Channel.from(params.library_size.collect{it.toInteger()})
+
+    sim_input = params.library_strategy == 'BOTH' ? REFORMAT_FASTA.out.wrap_fasta.combine(library_size).combine(Channel.from(['PE', 'SE'])) : REFORMAT_FASTA.out.wrap_fasta.combine(library_size).combine(Channel.from([params.library_strategy]))
+
+    GENERATE_SIMULATED_RNA_DATA(sim_input)
 
 }
-
